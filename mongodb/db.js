912fffb160391380d517e91b139770e0e6876056
sumitdev <sumit-dev1611>
2017-07-21 14:51:25 +0530

    address populate

--- mongodb/db.js
+++ mongodb/db.js
@@ -26,27 +26,21 @@ module.exports = function() {
     });
 
     var user_address = mongoose.Schema({
-        user_id: { type: String, required: true },
-        address: [{
-            city: String,
-            state: String,
-            pin_code: Number
-        }],
+        user_id: { type: String, required: true ,ref:'users_model'},
+        address: {
+            city:String,
+            state:String,
+            pin_code:Number
+        },
         phone_no: Number
     }, {
         collection: 'address',
         strict: true
     });
 
-    // var address_Schema = mongoose.Schema({
-    //     city: String,
-    //     state: String,
-    //     pin_code: Number
-    // });
-
-    var users_model = conn.model('users', details);
-    var access_token_model = conn.model('access_token', access_detail);
-    var user_address_model = conn.model('address', user_address);
+    var users_model = conn.model('users_model', details);
+    var access_token_model = conn.model('access_token_model', access_detail);
+    var user_address_model = conn.model('user_address_model', user_address);
 
     return function(req, res, next) {
         req.users_collection = users_model;
--- routes/index.js
+++ routes/index.js
@@ -80,12 +80,20 @@ router.post('/user/login/', function(req, res, next) {
 });
 
 
-router.get('/user/get/:access_token', function(req, res, next) {
-    req.users_collection.findOne({ _id: req.params.access_token }, function(err, data) {
+router.get('/user/get', function(req, res, next) {
+    req.access_token_collection.findOne({ access_token: req.query.access_token }, function(err, data) {
         if (err) {
             next(err);
         } else if (data) {
-            res.json(data)
+            req.address_collection.find({user_id: data.user_id }).populate('user_id').exec(function(err, complete_data) {
+                if (err) {
+                    next(err);
+                } else if (complete_data) {
+                    res.json(complete_data)
+                } else {
+                    res.json("can't fetch data")
+                }
+            });
         } else {
             res.json('data not found');
         }
@@ -116,7 +124,8 @@ router.get('/user/list/:page', function(req, res, next) {
 });
 
 router.post('/user/address/:access_token', function(req, res, next) {
-    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     validation.validateAddress(req.body, function(err, data) {
         if (err) {
             next(err);
