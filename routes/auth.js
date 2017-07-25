263e66983159bdc7f292cc988e8d69f3a8980ae9
sumitdev <sumit-dev1611>
2017-07-25 10:43:30 +0530

    Jwt assignment

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-     verifyAccess : (req, callback) =>{
+    verifyAccess: (req, callback) => {
         req.access_token_collection.findOne({ access_token: req.query.access_token }, function(err, access_token_data) {
             if (err) {
                 next(err);
@@ -8,4 +8,5 @@ module.exports = {
             }
         });
     }
+
 }
